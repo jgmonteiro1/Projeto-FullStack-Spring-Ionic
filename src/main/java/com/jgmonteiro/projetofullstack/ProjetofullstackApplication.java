edef0cc734b205b1ecb37d31d32822337658b714
João Gabriel <jgmoreira15@gmail.com>
2020-03-13 02:49:19 -0300

    Projeto criado

---
+++ src/main/java/com/jgmonteiro/projetofullstack/ProjetofullstackApplication.java
@@ -0,0 +1,13 @@
+package com.jgmonteiro.projetofullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetofullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetofullstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jgmonteiro/projetofullstack/ProjetofullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgmonteiro.projetofullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetofullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
