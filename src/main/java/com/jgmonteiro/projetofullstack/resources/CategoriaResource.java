23512f73ac2b278bb8a20ecbf4050b408d100876
João Gabriel <jgmoreira15@gmail.com>
2020-03-13 03:39:11 -0300

    Teste do Rest CategoriaResource e criação da classe de dominio Categoria

---
+++ src/main/java/com/jgmonteiro/projetofullstack/resources/CategoriaResource.java
@@ -0,0 +1,28 @@
+package com.jgmonteiro.projetofullstack.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jgmonteiro.projetofullstack.domain.Categoria;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+	
+	@GetMapping
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> list = new ArrayList<>();
+		
+		list.addAll(Arrays.asList(cat1,cat2));
+		return list;
+	}
+}
