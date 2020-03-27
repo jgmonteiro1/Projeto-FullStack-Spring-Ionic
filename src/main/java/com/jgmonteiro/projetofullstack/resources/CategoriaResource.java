b8db95c72e083ea51b7115e9a241a0c73bc21c8b
João Gabriel <jgmoreira15@gmail.com>
2020-03-27 20:33:34 -0300

    Criação do método POST para inserir uma nova categoria no CategoriaResource + CategoriaService

--- src/main/java/com/jgmonteiro/projetofullstack/resources/CategoriaResource.java
+++ src/main/java/com/jgmonteiro/projetofullstack/resources/CategoriaResource.java
@@ -1,14 +1,18 @@
 package com.jgmonteiro.projetofullstack.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jgmonteiro.projetofullstack.domain.Categoria;
 import com.jgmonteiro.projetofullstack.service.CategoriaService;
@@ -32,7 +36,13 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(list);
 	}
 	
-	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+		
+	}
 	
 	
 }
--- src/main/java/com/jgmonteiro/projetofullstack/service/CategoriaService.java
+++ src/main/java/com/jgmonteiro/projetofullstack/service/CategoriaService.java
@@ -26,4 +26,9 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
 }
