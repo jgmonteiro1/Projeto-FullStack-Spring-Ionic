d4b9125ce032f450b93360f30125492e1b15b53e
João Gabriel <jgmoreira15@gmail.com>
2020-03-27 21:13:35 -0300

    Criação do método PUT no CategoriaResource e CategoriaService

--- src/main/java/com/jgmonteiro/projetofullstack/resources/CategoriaResource.java
+++ src/main/java/com/jgmonteiro/projetofullstack/resources/CategoriaResource.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -44,6 +45,11 @@ public class CategoriaResource {
 		
 	}
 	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
 	
 }
  
--- src/main/java/com/jgmonteiro/projetofullstack/service/CategoriaService.java
+++ src/main/java/com/jgmonteiro/projetofullstack/service/CategoriaService.java
@@ -31,4 +31,9 @@ public class CategoriaService {
 		obj.setId(null);
 		return repository.save(obj);
 	}
+	
+	public Categoria update(Categoria obj) {
+		findById(obj.getId());
+		return repository.save(obj);
+	}
 }
