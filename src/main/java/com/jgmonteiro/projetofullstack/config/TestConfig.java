8083bae70ebb9924d307999d546d60f230f75d2e
João Gabriel <jgmoreira15@gmail.com>
2020-06-29 16:47:42 -0300

    Criação do profile de testes + Refatoração da classe de configurações testConfig + Criação da classe DBService

--- src/main/java/com/jgmonteiro/projetofullstack/config/TestConfig.java
+++ src/main/java/com/jgmonteiro/projetofullstack/config/TestConfig.java
@@ -1,168 +1,33 @@
 package com.jgmonteiro.projetofullstack.config;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
+import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import com.jgmonteiro.projetofullstack.domain.Categoria;
-import com.jgmonteiro.projetofullstack.domain.Cidade;
-import com.jgmonteiro.projetofullstack.domain.Cliente;
-import com.jgmonteiro.projetofullstack.domain.Endereco;
-import com.jgmonteiro.projetofullstack.domain.Estado;
-import com.jgmonteiro.projetofullstack.domain.ItemPedido;
-import com.jgmonteiro.projetofullstack.domain.Pagamento;
-import com.jgmonteiro.projetofullstack.domain.PagamentoComBoleto;
-import com.jgmonteiro.projetofullstack.domain.PagamentoComCartao;
-import com.jgmonteiro.projetofullstack.domain.Pedido;
-import com.jgmonteiro.projetofullstack.domain.Produto;
-import com.jgmonteiro.projetofullstack.domain.enums.EstadoPagamento;
-import com.jgmonteiro.projetofullstack.domain.enums.TipoCliente;
-import com.jgmonteiro.projetofullstack.repository.CategoriaRepository;
-import com.jgmonteiro.projetofullstack.repository.CidadeRepository;
-import com.jgmonteiro.projetofullstack.repository.ClienteRepository;
-import com.jgmonteiro.projetofullstack.repository.EnderecoRepository;
-import com.jgmonteiro.projetofullstack.repository.EstadoRepository;
-import com.jgmonteiro.projetofullstack.repository.ItemPedidoRepository;
-import com.jgmonteiro.projetofullstack.repository.PagamentoRepository;
-import com.jgmonteiro.projetofullstack.repository.PedidoRepository;
-import com.jgmonteiro.projetofullstack.repository.ProdutoRepository;
+import com.jgmonteiro.projetofullstack.service.DBService;
 
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
-	private CategoriaRepository categoriaRepository;
-	
-	@Autowired
-	private ProdutoRepository produtoRepository;
-	
-	@Autowired
-	private EstadoRepository estadoRepository;
-	
-	@Autowired
-	private CidadeRepository cidadeRepository;
-	
-	@Autowired
-	private ClienteRepository clienteRepository;
-	
-	@Autowired
-	private EnderecoRepository enderecoRepository;
-	
-	@Autowired
-	private PedidoRepository pedidoRepository;
-	
-	@Autowired
-	private PagamentoRepository pagamentoRepository;
-	
-	@Autowired
-	private ItemPedidoRepository itemPedidoRepository;
+	private DBService dbService;
 	
 	@Override
 	public void run(String... args) throws Exception {
 				
-		Categoria cat1 = new Categoria(null, "Informática");
-		Categoria cat2 = new Categoria(null, "Escritório");
-		Categoria cat3 = new Categoria(null, "Frutas");
-		Categoria cat4 = new Categoria(null, "Doces");
-		Categoria cat5 = new Categoria(null, "Perfumaria");
-		Categoria cat6 = new Categoria(null, "Carros");
-		Categoria cat7 = new Categoria(null, "Motos");
+		
 
-
-		Produto p1 = new Produto(null, "Computador", 2000.00);
-		Produto p2 = new Produto(null, "Impressora", 800.00);
-		Produto p3 = new Produto(null, "Mouse", 80.00);
-		
-		Produto p4 = new Produto(null, "Mesa de escritório", 300.00);
-		Produto p5 = new Produto(null, "Toalha", 50.00);
-		Produto p6 = new Produto(null, "Colcha", 200.00);
-		Produto p7 = new Produto(null, "TV true collor", 1200.00);
-		Produto p8 = new Produto(null, "Roçadeira", 800.00);
-		Produto p9 = new Produto(null, "Abajour", 100.00);
-		Produto p10 = new Produto(null, "Pendente", 180.00);
-		Produto p11 = new Produto(null, "Shampoo", 90.00);
-		
-		Estado est1 = new Estado(null, "Bahia");
-		Estado est2 = new Estado(null, "São Paulo");
-		
-		Cidade c1 = new Cidade(null, "Salvador", est1);
-		Cidade c2 = new Cidade(null, "Lauro de Freitas", est1);
-		Cidade c3 = new Cidade(null, "São Paulo", est2);
-		
-		est1.getCidades().addAll(Arrays.asList(c1,c2));
-		est2.getCidades().add(c3);
-		
-		
-		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
-		cat2.getProdutos().addAll(Arrays.asList(p2,p4));
-		cat3.getProdutos().addAll(Arrays.asList(p5,p6));
-		cat4.getProdutos().addAll(Arrays.asList(p1,p2,p3,p7));
-		cat5.getProdutos().add(p8);
-		cat6.getProdutos().addAll(Arrays.asList(p9,p10));
-		cat7.getProdutos().add(p11);
-		
-		p1.getCategorias().addAll(Arrays.asList(cat1, cat4));
-		p2.getCategorias().addAll(Arrays.asList(cat1,cat2, cat4));
-		p3.getCategorias().addAll(Arrays.asList(cat1, cat4));
-		p4.getCategorias().add(cat2);
-		p5.getCategorias().add(cat3);
-		p6.getCategorias().add(cat3);
-		p7.getCategorias().add(cat4);
-		p8.getCategorias().add(cat5);
-		p9.getCategorias().add(cat6);
-		p10.getCategorias().add(cat6);
-		p11.getCategorias().add(cat7);
-		
-		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
-		cli1.getTelefones().addAll(Arrays.asList("40028922","30221515"));
-		
-		Endereco e1 = new Endereco(null, "Rua Flores", "14", "Casa dos fundos", "Nordeste", "38220834", cli1, c1);
-		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
-		
-		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
-		
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3,cat4,cat5,cat6,cat7));
-		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
-		estadoRepository.saveAll(Arrays.asList(est1,est2));
-		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1,e2));
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-		
-		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
-		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
-		
-		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
-		ped1.setPagamento(pagto1);
-		
-		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
-		ped2.setPagamento(pagto2);
-		
-		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
-		
-		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
-		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
-		
-		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
-		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
-		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
-		
-		ped1.getItens().addAll(Arrays.asList(ip1,ip2));
-		ped2.getItens().addAll(Arrays.asList(ip3));
-		
-		p1.getItens().addAll(Arrays.asList(ip1));
-		p2.getItens().addAll(Arrays.asList(ip3));
-		p3.getItens().addAll(Arrays.asList(ip2));
-		
-		itemPedidoRepository.saveAll(Arrays.asList(ip1,ip2,ip3));
-
+	}
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		dbService.instantiateTestDataBase();
+		return true;
 	}
 
 }
